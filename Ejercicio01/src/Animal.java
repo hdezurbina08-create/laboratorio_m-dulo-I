a34c0b546fc540ec37941c358a1e5a8c214d7f55
hdezurbina08-create <hdez.urbina08@gmail.com>
2026-08-03 12:54:10 -0600

    realice Ejercicio 1-Sistema de animales

---
+++ Ejercicio01/src/Animal.java
@@ -0,0 +1,10 @@
+public abstract class Animal {
+
+    protected String nombre;
+
+    public Animal(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public abstract void hacerSonido();
+}
---
+++ Ejercicio01/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal perro = new Perro("Firulais");
+        Animal gato = new Gato("Michi");
+
+        perro.hacerSonido();
+        gato.hacerSonido();
+    }
+}
